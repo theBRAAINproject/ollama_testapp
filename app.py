5779957afda85c67d5cb66a9b16ec77cfff3e233
rizMehdi <ccmeall@gmail.com>
2025-12-16 20:40:53 +0000

    streamlit output

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 st.write("hello world")
 from ollama import chat
 from ollama import Client
+from ollama import generate
 
 
 client = Client(
@@ -21,11 +22,24 @@ client = Client(
 # for part in client.chat('gpt-oss:120b', messages=messages, stream=True):
 #   print(part['message']['content'], end='', flush=True)
 
-response = chat(
-  model='gpt-oss:120b',
-  messages=[{'role': 'user', 'content': 'Tell me about Canada.'}],
-)
-print(response.message.content)
+# response = chat(
+#   model='gpt-oss:120b',
+#   messages=[{'role': 'user', 'content': 'Tell me about Canada.'}],
+# )
+# print(response.message.content)
+
+
+text = """
+Ollama lets users run large language models on local machines.
+This approach emphasizes privacy and control, as data does not leave the user's environment.
+Developers can leverage various open-source models through a simple interface, improving efficiency and reducing costs.
+"""
+
+
+prompt = f"Summarize the following text in one sentence:\n\"\"\"\n{text}\n\"\"\""
+result = generate(model='gpt-oss:120', prompt=prompt)
+print("Summary:", result['response'])
+st.print("Summary:", result['response'])
 
 
 
