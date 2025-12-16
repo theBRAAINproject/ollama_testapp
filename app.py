36c594216aba21e548c2e57ad03bf235e3c667b0
rizMehdi <ccmeall@gmail.com>
2025-12-16 16:32:33 +0000

    test ollama

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
 
 import streamlit as st
 st.write("hello world")
+
+from ollama import Client
+# ollama signin
+ollama pull gpt-oss:120b-cloud
+
+
+client = Client(
+    host="https://ollama.com",
+    headers={'Authorization': 'Bearer ' + os.environ.get('OLLAMA_API_KEY')}
+)
+
+messages = [
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?',
+  },
+]
+
+for part in client.chat('gpt-oss:120b', messages=messages, stream=True):
+  print(part['message']['content'], end='', flush=True)
